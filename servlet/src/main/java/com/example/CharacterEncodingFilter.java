a8e6abbb4413ca62bac0f792b0096286ad02e3b6
Seeun Park <94965944+sure-why-not@users.noreply.github.com>
2022-10-11 12:29:06 +0900

    Servlet 학습 테스트 (#3)
    * test: 1단계 - 서블릿 학습 테스트
    
    * test: 2단계 - 필터 학습 테스트
    
    * test: 필터에서 response 인코딩 처리 수정

--- servlet/src/main/java/com/example/CharacterEncodingFilter.java
+++ servlet/src/main/java/com/example/CharacterEncodingFilter.java
@@ -4,6 +4,9 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import javax.sound.sampled.AudioFormat.Encoding;
+import org.apache.tomcat.util.buf.Utf8Encoder;
 
 @WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
@@ -12,5 +15,7 @@ public class CharacterEncodingFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
         chain.doFilter(request, response);
+        request.getServletContext().log("doFilter() 돌아옴");
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
     }
 }
